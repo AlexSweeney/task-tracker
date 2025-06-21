c011e119ab1ecb846868bada09d25febf5c531a4
Alex Sweeney <a.sweeney@ryte.com>
2025-06-21 16:21:06 +0100

    add task from empty

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const readline = require('readline');
+const fs = require('fs');
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -12,15 +13,35 @@ rl.prompt();
 rl.on('line', (line) => {
   const input = line.trim();
 
-  switch(input) {
+  const [command, ...args] = input.split(' ');
+
+  switch(command) {
+  case 'add':  
+    const task = args.join(' '); 
+
+    const data = {
+      0: task
+    };
+
+    const jsonData = JSON.stringify(data, null, 2);
+
+    if(task) { 
+      fs.writeFileSync('tasks.json', jsonData)
+    } else {
+      console.log(`task: ${task} not added`)
+      console.log('please enter a name for the task, ie: add do dishes')
+    }
+    
+    break;
+  case 'exit': 
+    rl.close();
+    return
   case 'help': 
     console.log(`available commands:
       help - list all avaialbe commands
       exit - exit task-cli`)
     break;
-  case 'exit': 
-    rl.close();
-    return
+ 
   default: 
       console.log(`Unknown command: "${input}" enter "help" to see all avaialbe commands`)
   }
